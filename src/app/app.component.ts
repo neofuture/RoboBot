f6291f3adfcf966fa4bc93828f4560a7cbe2c327
Carl Fearby <carlfearby@me.com>
2023-02-10 22:04:15 +0000

    Code optimisations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,8 +23,7 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   sendQuestion() {
     const time = new Date().getTime().toString();
-    let question = {text: this.question, type: 'user', time};
-    this.conversationLog.push(question);
+    this.conversationLog.push({text: this.question, type: 'user', time});
     this.answering = true;
 
     setTimeout(() => {
@@ -34,7 +33,7 @@ export class AppComponent implements OnInit, AfterViewInit {
     setTimeout((question: any) => {
       this.answering = false;
       this.answerQuestion(question);
-    }, 1200, this.question);
+    }, 1600, this.question);
 
     this.question = '';
     this.input?.nativeElement.focus();
@@ -44,72 +43,75 @@ export class AppComponent implements OnInit, AfterViewInit {
     const time = new Date().getTime().toString();
     let answer: ConversationModel;
 
-    const searcherAnswer = fuzzysort.go(question.replace(/['"]/g, ''), this.questionsList, {key: 'question'});
-    const searcherQuestion = fuzzysort.go(question.replace(/['"]/g, ''), this.questionsList, {key: 'answer'});
+    const cleanedQuestion = question.replace(/['"]/g, '');
+
+    const [searcherAnswer, searcherQuestion] = [
+      fuzzysort.go(cleanedQuestion, this.questionsList, {key: 'question'}),
+      fuzzysort.go(cleanedQuestion, this.questionsList, {key: 'answer'})
+    ];
 
     if (searcherAnswer[0] !== undefined) {
-      let text = searcherAnswer[0].obj.answer;
-      if (searcherAnswer[1] !== undefined) {
-        if(
-          (searcherAnswer[0].obj.answer !== searcherAnswer[1].obj.answer) &&
-          searcherAnswer[1].score > -0.8
-        ) {
-          text += ', or it may be ' +
-            searcherAnswer[1].obj.answer + searcherAnswer[1].score;
-        }
+      const [text1, text2] = [searcherAnswer[0].obj.answer, searcherAnswer[1]?.obj.answer];
+
+      let text = text1;
+      if (text2 && text1 !== text2 && searcherAnswer[1].score > -0.8) {
+        text += `, or it may be ${text2}`;
       }
+
       text = text.replace('{{TIME}}', new Date().toLocaleTimeString());
       text = text.replace('{{DATE}}', new Date().toLocaleDateString());
 
       answer = {text, type: 'bot', time};
     } else if (searcherQuestion[0] !== undefined) {
-      let text = 'I have found this question which might serve as an answer. ' +
-        searcherQuestion[0].obj.question +
-        ' The answer being ' +
-        searcherQuestion[0].obj.answer +
-        '.';
+      const [q, a] = [searcherQuestion[0].obj.question, searcherQuestion[0].obj.answer];
+      const text = `I have found this question which might serve as an answer. ${q} The answer being, ${a}.`;
 
       answer = {text, type: 'bot', time};
     } else {
       const calculation = question.replace(/[^0-9\+\-\*\/\.]/g, '');
-      if (math.evaluate(calculation) === undefined) {
+      const calcResult = math.evaluate(calculation);
+      if (calcResult === undefined) {
         answer = {text: 'Could you please rephrase your question', type: 'bot', time};
       } else {
-        answer = {text: `I have calculated that to be ${math.evaluate(calculation)}`, type: 'bot', time};
+        answer = {text: `I have calculated that to be ${calcResult}`, type: 'bot', time};
       }
     }
 
     this.conversationLog.push(answer);
+
     setTimeout(() => {
       this.scrollToBottom();
     }, 100);
   }
 
   shuffleArray() {
-    const array = this.questionsList
-    for (let i = array.length - 1; i > 0; i--) {
+    const array = this.questionsList;
+    const arrayLength = array.length;
+    for (let i = arrayLength - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+      array.splice(i, 1, array.splice(j, 1, array[i])[0]);
     }
     return array;
   }
 
   ngOnInit() {
-    let exampleQuestions = []
-    for(let item of this.shuffleArray().slice(0, 3)) {
-      exampleQuestions.push(`<li>${item.question}</li>`);
-    }
+    this.answering = true;
+    const exampleQuestions = this.shuffleArray().slice(0, 3)
+      .map(item => `<li>${item.question}</li>`)
+      .join('');
 
-    this.conversationLog.push({
-      text: 'Hello and welcome<br><br>Please ask me some questions' +
-        ', you can ask my name, what the time is, the date, or ask ' +
-        'me to do some simple calculations<br><br>Some examples<br>' +
-        '<ul>' + exampleQuestions.join('')+ '</ul>I also use fuzzy ' +
-        'logic, so you can ask me questions in different ways, I can' +
-        ' also attept to for the question if you ask me the answer',
-      type: 'bot',
-      time: new Date().getTime().toString()
-    });
+    setTimeout(() => {
+      this.answering = false;
+      this.conversationLog.push({
+        text: `Hello and welcome<br><br>Please ask me some questions,
+              you can ask my name, what the time is, the date, or ask me to do some simple
+              calculations<br><br>Some examples<br><ul>${exampleQuestions}</ul>
+              I also use fuzzy logic, so you can ask me questions in different ways,
+              I can also attept to for the question if you ask me the answer`,
+        type: 'bot',
+        time: new Date().getTime().toString()
+      });
+    }, 2500);
   }
 
   ngAfterViewInit(): void {
